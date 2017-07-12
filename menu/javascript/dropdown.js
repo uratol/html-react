cb9bca19065f0faee4d68f7185f2130263ad1d32
Юрий Толкунов <ukr-tolkunov@ukr.eldorado.local>
2017-07-12 18:59:04 +0300

    menu

--- menu/javascript/dropdown.js
+++ menu/javascript/dropdown.js
@@ -1,6 +1,20 @@
 $( document ).ready(function() {
+
+    function showDropdownContent(elem){
+        var left = elem.parent().offset().left;
+
+        var rect = elem[0].getBoundingClientRect();
+
+        if (left + elem.width() > window.innerWidth)
+            left = window.innerWidth - rect.width;
+        elem.css({left: left});
+    }
+
     $('.dropbtn').click(function(){
-        $(this).closest('.dropdown').find('.dropdown-content').first().toggleClass('dropdown-show');
+        var dropdownContent = $(this).closest('.dropdown').find('.dropdown-content').first();
+        dropdownContent.toggleClass('dropdown-show');
+        if (dropdownContent.hasClass('dropdown-show'))
+            showDropdownContent(dropdownContent);
     });
 
 // Close the dropdown menu if the user clicks outside of it
